6fc6c25b407385eb6eee5a625c9fddbd8530002a
HottBraz <joao.hott@mail.uft.edu.br>
2025-10-23 21:11:15 -0300

    Comit da aula

--- sistema/urls.py
+++ sistema/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from sistema.views import Login , Logout
+from sistema.views import Login, LoginAPI , Logout
 
 urlpatterns = [
     path('', Login.as_view(), name='login'),
@@ -24,4 +24,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('veiculo/', include('veiculo.urls'), name='veiculo'),
     path('Logout/', Logout.as_view(), name='logout'),
+    path('autenticacao-api/', LoginAPI.as_view())
     ]
--- sistema/views.py
+++ sistema/views.py
@@ -3,6 +3,9 @@ from django.contrib.auth import authenticate, login, logout
 from django.views.generic import View
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.authtoken.models import Token
+from rest_framework.response import Response
 
 
 
@@ -30,6 +33,24 @@ class Login(View):
                 return redirect("/veiculo")
         else:
             return render(request, 'autenticacao.html', {'mensagem': 'Usuário ou senha inválidos!'})
+
+class LoginAPI(ObtainAuthToken):
+
+    def post(self, request, *args, **kwargs):
+        response = super(LoginAPI, self).post(request, *args, **kwargs)
+        serializer = self.serializer_class(data=request.data,
+                                           context={'request': request}
+                                           )
+        serializer.is_valid(raise_exception=True)
+        user = serializer.validated_data['user']
+        token, created = Token.objects.get_or_create(user=user)
+        return Response({
+            'id': user.id,
+            'nome': user.firt_name,
+            'email': user.email,
+            'token': token.key
+        })
+
 
 class Logout(View):
     """
